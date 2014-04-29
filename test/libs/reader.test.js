3feaa1cdf53ad9749d9311d44a34ef9418b75c57
Antonin Januska <antonin@antjanus.com>
2014-04-28 19:54:37 -0500

    A few tests to ensure things work

--- test/libs/reader.test.js
+++ test/libs/reader.test.js
@@ -9,29 +9,43 @@ var reader = new Reader({
 describe('Reader', function() {
   describe('Find file', function() {
     it('should return a path to a file', function(done) {
-      var slugPath = '/posts/first-post';
+      var slugPath = ['posts','first-post'];
       var filePath = reader.findFile(slugPath);
 
-      filePath.should.eql(['content', '1-posts', '1-first-post']);
+      filePath.should.eql(['test/content', '1-posts', '1-first-post']);
       done();
     });
   });
 
   describe('Get file', function() {
     it('should return a path to a file', function(done) {
-      var slugPath = '/posts/first-post';
+      var slugPath = ['posts','first-post'];
       var filePath = reader.getFile(slugPath);
+
+      filePath.should.eql(
+        { title: 'Something',
+          content: 'else',
+          path: [ 'test/content', '1-posts', '1-first-post' ],
+          slug: [ 'posts', 'first-post' ] });
+      done();
+    });
+  });
+
+  describe('Get children of a file', function() {
+    it('should return information on children of a file', function(done) {
+      var slugPath = '/posts/first-post';
+      var filePath = reader.getChildren(slugPath);
 
       filePath.should.eql();
       done();
     });
   });
+  describe('Find children of a file', function() {
+    it('should return path information on children of a file', function(done) {
+      var slugPath = '/posts/first-post';
+      var filePath = reader.findChildren(slugPath);
 
-  describe('Post Title', function() {
-    it('should return a list of title', function(done) {
-      var titleList = reader.getPostTitles();
-
-      titleList[0].should.eql('Testing title');
+      filePath.should.eql();
       done();
     });
   });
