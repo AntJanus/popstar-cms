2ba3ff064b68c9a04a1a7f821d5a3f37772ba4e2
Antonin Januska <antonin@antjanus.com>
2014-07-10 23:30:46 -0500

    Starting hippie test

---
+++ test/api/routes.js
@@ -0,0 +1,31 @@
+var hippie = require('hippie');
+var compareJSON = require('json-structure-validator');
+
+var api = function() {
+  return hippie().json().base('http://localhost:3000?format=json');
+};
+
+var emptyObj = {
+
+};
+
+hippie.assert.showDiff = true;
+
+describe('routes', function() {
+  it('should return full site object', function(done) {
+    api()
+      .get('/')
+    .expectStatus('200')
+    .expectHeader('Content-type', 'application/json; charset=utf-8')
+    .expect(function(res, body, next) {
+      var comparison = compareJSON(emptyObj, body);
+      if (comparison === true) {
+        next();
+      } else {
+        throw new Error(comparison);
+      }
+    })
+    .end(done)
+  })
+
+});
