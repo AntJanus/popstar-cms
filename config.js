36dd8bfed64ad4967d1e4a9233ce02d663c46573
Antonin Januska <antonin@antjanus.com>
2015-07-13 15:39:14 -0600

    Update

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 //site config
+var path = require('path');
+
 module.exports = {
   reader: {
-    postsPerPage: 5
+    postsPerPage: 5,
+    directory: path.join(__dirname, 'content')
   },
   cache: {
     enabled: false
   },
   site: {
     title: 'Popstar CMS',
-  }
+  },
 };
